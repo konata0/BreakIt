010799c6cf836de719ccd8d9b63826d573068788
konata0 <xyhkonata@gmail.com>
2020-03-06 22:39:16 +0800

    set game background

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour{
+    public Sprite[] spriteList;
+    // Start is called before the first frame update
+    private List<List<int>> levelData = null;
+    private int levelBackground = 0;
+    private GameObject background;
+    void Start(){
+        Globle.readLevelData();
+        // 获取关卡数据
+        levelData = Globle.getLevelData();
+        levelBackground = Mathf.Clamp(Globle.getLevelBackground(), 0, spriteList.Length - 1);
+        // 设置背景
+        background = GameObject.Find("Background");
+        background.GetComponent<SpriteRenderer>().sprite = spriteList[levelBackground];
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
--- Assets/Scripts/Globle.cs
+++ Assets/Scripts/Globle.cs
@@ -1,8 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using LitJson;
 
 public class Globle : MonoBehaviour{
-    
+    public static JsonData levelData;
+    public static int row = 6;
+    public static int col = 10;
+    public static int levelNumber = 1;
+    public static int levelIndex = 0;
+
+    // 从JSON文件读取关卡数据
+    public static void readLevelData(){
+        StreamReader streamReader = new StreamReader(Application.dataPath + @"/GameData/LevelData.json");
+        string str = streamReader.ReadToEnd();
+        levelData = JsonMapper.ToObject(str);
+    }
+
+    // 获取关卡数据
+    public static List<List<int>> getLevelData(){
+        List<List<int>> re = new List<List<int>>();
+        for(int i = 0; i <= row - 1; i++){
+            re.Add(new List<int>());
+            for(int j = 0; j <= col - 1; j++){
+                re[i].Add((int)(levelData["level" + levelIndex.ToString()]["data"][i * col + j]));
+            }
+        }   
+        return re;
+    }
+    // 获取关卡背景设置
+    public static int getLevelBackground(){  
+        return (int)(levelData["level" + levelIndex.ToString()]["bg"]);
+    }
 
 }
